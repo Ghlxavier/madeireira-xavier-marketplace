b03a66a8bb21dc1848122174bc06982227d0585e
brocolis <162514135+Ghlxavier@users.noreply.github.com>
2026-04-16 14:00:53 -0300

    Add files via upload

---
+++ dist/organizar_noticias.js
@@ -0,0 +1,30 @@
+import { db } from "./db.js";
+import { noticia } from "./schema.js";
+import { asc, desc } from "drizzle-orm";
+import { rl } from "./menu.js";
+export async function listarNoticias(ordem) {
+    const noticias = await db
+        .select()
+        .from(noticia)
+        .orderBy(ordem === "ASC" ? asc(noticia.dataCriacao) : desc(noticia.dataCriacao));
+    if (noticias.length === 0) {
+        console.log("Nenhuma notícia cadastrada.");
+        return;
+    }
+    for (const item of noticias) {
+        console.log("\n----------------------");
+        console.log(`Título: ${item.titulo}`);
+        console.log(`Conteúdo: ${item.texto}`);
+        console.log(`Data: ${item.dataCriacao}`);
+    }
+    console.log("\nTotal de notícias:", noticias.length);
+    let sair = false;
+    console.log("\n(z)-Voltar");
+    while (!sair) {
+        let voltarMenu = (await rl.question("> ")).trim();
+        if (voltarMenu.toLowerCase() === "z") {
+            return;
+        }
+    }
+}
+//# sourceMappingURL=organizar_noticias.js.map
